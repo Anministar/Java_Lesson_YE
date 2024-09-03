15bab428a03b1424ed3cf4bd8fd8348d27570a36
Anministar <ldh1081703@gmail.com>
2024-09-04 01:44:17 +0900

    2024/09/04 WED

---
+++ src/Ch10.java
@@ -0,0 +1,72 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Ch10 {
+	public static void main(String[] args) {
+		// 중첩 while문
+		// 예제 01)
+		// 구구단 2단 출력
+		
+//		System.out.println(2 + " X " + 1 + " = " + 2 * 1);
+//		System.out.println(2 + " X " + 2 + " = " + 2 * 2);
+//		System.out.println(2 + " X " + 3 + " = " + 2 * 3);
+//		System.out.println(2 + " X " + 4 + " = " + 2 * 4);
+//		System.out.println(2 + " X " + 5 + " = " + 2 * 5);
+		
+
+
+		
+		//높이 : 4
+		//****
+		//***
+		//**
+		//*
+		
+		
+
+		//4
+		//   *
+		//  ***
+		// *****
+		//*******
+		
+		
+		//높이 : n
+		//   *
+		//  ***
+		// *****
+		//*******
+		
+		
+
+		
+		//5
+		//*******
+		// *****
+		//  ***
+		//   *
+		
+		
+		
+		//높이 : n
+		//*******
+		// *****
+		//  ***
+		//   *
+
+		
+		
+		
+		//6
+		//   *
+		//  ***
+		// *****
+		//*******
+		// *****
+		//  ***
+		//   *
+				
+					
+		
+	}
+
+}
---
+++ src/Ch13/C02PersonMain.java
@@ -0,0 +1,18 @@
+package Ch13;
+
+ class C02Person {
+	String name;
+	int age;
+	String addr;
+}
+public class C02PersonMain {
+	public static void main(String[] args) {
+		C02Person hong = new C02Person();
+		hong.name = "홍길동";
+		hong.age = 55;
+		hong.addr = "대구대구대구";
+		
+		System.out.printf("%s %d %d\n", hong.name, hong.age, hong.addr);
+	}
+
+}
