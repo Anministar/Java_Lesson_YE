2a3332ab6df4f2b5834a5cfd0da69a68e9837bf7
이동하 <ldh1081703@gmail.com>
2024-09-11 22:28:58 +0900

    test

---
+++ src/Ch14/hihi.java
@@ -0,0 +1,14 @@
+package Ch14;
+
+public class hihi {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+
+}
